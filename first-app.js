87529db6c47eff5d801eda0b458adb3d002dfbb9
Olubukola Akintoroye <Olubukola.Akintoroye@gmail.com>
2018-12-17 23:36:42 -0500

    Learned importing file system functionalities, and createing a file within current file

--- first-app.js
+++ first-app.js
@@ -1,1 +1,6 @@
-console.log('Hello from Node.js');
+//console.log('Hello from Node.js');
+//import file system functionalities
+//const fs = require('fs'); 
+// creates a file within current folder
+//fs.writeFileSync('hello.txt','hello from Node.js'); 
+
