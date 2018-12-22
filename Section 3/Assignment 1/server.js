2fbf00782e36ce83b0c5d4ca901fcb8f2813589a
Olubukola Akintoroye <Olubukola.Akintoroye@gmail.com>
2018-12-22 17:44:04 -0500

    Added Notes to Section 3 Assignment 1

--- Section 3/Assignment 1/server.js
+++ Section 3/Assignment 1/server.js
@@ -1,8 +1,12 @@
 
+//import http core module
 const http = require('http');
 
+//import user.js file
 const userpage = require('./users');
 
+//create a server by calling function found in userpage
 const server = http.createServer(userpage);
 
+//listen to server on port 3000
 server.listen(3000);
--- Section 3/Assignment 1/users.js
+++ Section 3/Assignment 1/users.js
@@ -2,34 +2,23 @@ const fs = require('fs');
 
 const requestHandler = (req,res)=>{
     const url = req.url;
-    const method = req.method;
 
     if(url === '/'){
+        /**
+         * Add header - Suggested from Assignment Solution
+         */
+        res.setHeader('Content-Type','text/html');
         res.write('<html><head>Hello from the other side :D!<body>');
         res.write('<form action="/create-user" method="POST"><input type="text" name = "username"><button type="submit">Send</button></input></form>');
         res.write('</body></head></html>');
 
         return res.end();
     }
-
-    if(url === '/create-user' && method === 'POST'){
-        const body = [];
-        req.on('data',(chunk) =>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end',() =>{
-        const parsedBody = Buffer.concat(body).toString();
-        const userN = parsedBody.split('=')[1];
-
-        console.log("Welcome " + userN);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-        });
-    }
-
     if(url === '/users'){
+        /**
+         * Add header - Suggested from Assignment Solution
+         */
+        res.setHeader('Content-Type','text/html');
         res.write('<html><head><body>');
         res.write('<ul><li>User 1</li></ul><br></br>');
         res.write('<ul><li>User 2</li></ul><br></br>');
@@ -39,5 +28,31 @@ const requestHandler = (req,res)=>{
 
         return res.end();
     }
+    if(url === '/create-user'){
+        const body = [];
+        //excuted for every new piece of new coming data
+        //without these few lines of code, could not possibly
+        //load the body array with text!
+        req.on('data',(chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        //will run after all the data has been ran!
+        return req.on('end',() =>{
+         //takes parsed text and converts to string
+        const parsedBody = Buffer.concat(body).toString();
+        //the first element of the parsed text will be the data
+        //we need (formatted as key = value before spliting)
+        const userN = parsedBody.split('=')[1];
+
+        console.log("Welcome " + userN);
+        res.statusCode = 302;
+        //set rediretion
+        res.setHeader('Location', '/');
+        return res.end();
+        });
+    }
+
+ 
 };
 module.exports = requestHandler;
