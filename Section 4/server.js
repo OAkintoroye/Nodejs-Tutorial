71aa89e7138fcc0410d9773f51164012628b4ed8
Olubukola Akintoroye <Olubukola.Akintoroye@gmail.com>
2018-12-23 17:04:48 -0500

    Notes from Section 4 of Tutorial. Main focus on debuggin in VS Code

---
+++ Section 4/server.js
@@ -0,0 +1,26 @@
+//How to create a server in node.js
+
+const http = require('http');
+
+const routes = require('./routes');
+
+
+//require is used to import files into nodejs 
+// ./ = relative path and / = absolute path
+
+//Three different ways to represent
+/*function rqListner(req,res){
+}
+http.createServer(rqListner);*/
+/*http.createServer(function(req,res){
+
+});
+*/
+const server = http.createServer(routes);
+/**
+ * In order to recieve an export (import) with multiple elements:
+ * 
+ * const server = http.createServer(routes.handler)
+ */
+server.listen(3000);
+//go to web browser and type into address bar : localhost:3000
